f1e4c165787957dcfe68101548968be0ec0f35f3
Nathen Harvey <nathen.harvey@gmail.com>
2012-04-02 10:35:16 -0400

    bump version to 0.0.6

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## v0.0.6
+
+* Add the monitoring recipe
+
+## v0.0.5
+
+* Current public release
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nharvey@customink.com"
 license          "Apache 2.0"
 description      "Installs Percona apt/yum repostiory, client, and server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 
 attribute = "percona-install/plugins_url", 
   :dislpay_name => "Percona Install - Plugins URL",
