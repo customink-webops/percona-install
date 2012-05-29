3e61a94c4d1b5e34c4968d6f5842962dfc4c28ad
Nathen Harvey <nathen.harvey@gmail.com>
2012-05-29 08:43:07 -0400

    bump to version 0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+### v.0.1.3
+
+* Add foodcritic tests monitored with [Travis CI](http://travis-ci.org/#!/customink-webops/percona-install)
+* Bump version to a more semantic number
+
 ## v0.0.6
 
 * Add the monitoring recipe
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nharvey@customink.com"
 license          "Apache 2.0"
 description      "Installs Percona apt/yum repostiory, client, and server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.1.3"
 
 recipe           "default", "Installs percona-release apt or yum repository"
 recipe           "client", "Installs Percona client"
